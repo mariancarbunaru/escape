c27e299163ad9e4827b8d89429558189d808c152
mariancarbunaru <86341980+mariancarbunaru@users.noreply.github.com>
2021-06-23 10:49:05 +0300

    Update header.js

--- surse/header.js
+++ surse/header.js
@@ -3,27 +3,27 @@
 export let player = { speed:1.95, turnSpeed:Math.PI*0.02, precision:9};
 export let unlockDoor = {x_min:2, x_max:5, y_min:-2, y_max:2, z:-70};
 export let unlockDoorL3 = {x_min:2, x_max:5, y_min:-20, y_max:0, z:165};
-export const l2Texture = 'http://localhost:8080/resources/images/backgrounds/bg_lev2.jpg';
-export const l0Texture = 'http://localhost:8080/resources/images/backgrounds/start_bg.jpg';
-export const l0Buttons = 'http://localhost:8080/resources/images/backgrounds/blue3_round.jpg';
-export const l0StartButton = 'http://localhost:8080/resources/images/backgrounds/blue3_start.jpg';
-export const l0StoryButton = 'http://localhost:8080/resources/images/backgrounds/blue3_story.jpg';
-export const l0Picture1 = 'http://localhost:8080/resources/images/backgrounds/3.png';
-export const l0Picture2 = 'http://localhost:8080/resources/images/backgrounds/9.png';
+export const l2Texture = 'resources/images/backgrounds/bg_lev2.jpg';
+export const l0Texture = 'resources/images/backgrounds/start_bg.jpg';
+export const l0Buttons = 'resources/images/backgrounds/blue3_round.jpg';
+export const l0StartButton = 'resources/images/backgrounds/blue3_start.jpg';
+export const l0StoryButton = 'resources/images/backgrounds/blue3_story.jpg';
+export const l0Picture1 = 'resources/images/backgrounds/3.png';
+export const l0Picture2 = 'resources/images/backgrounds/9.png';
 
 
 
 
-export const pathSounds = { ambient0:'http://localhost:8080/resources/sounds/ambient0.ogg',
-							ambient1:'http://localhost:8080/resources/sounds/ambient1.ogg', 
-							ambient2:'http://localhost:8080/resources/sounds/ambient2.ogg',
-							ambient3:'http://localhost:8080/resources/sounds/ambient3.ogg',
-							prisonDoor:'http://localhost:8080/resources/sounds/metal_barrier_open.ogg',
-							click:'http://localhost:8080/resources/sounds/click.ogg',
-							laugh:'http://localhost:8080/resources/sounds/laugh.ogg',
-							grab:'http://localhost:8080/resources/sounds/grab.ogg',
-							steps:'http://localhost:8080/resources/sounds/steps.ogg',
-							jump:'http://localhost:8080/resources/sounds/jump.ogg',
-							scream:'http://localhost:8080/resources/sounds/scream.ogg',
+// export const pathSounds = { ambient0:'http://localhost:8080/resources/sounds/ambient0.ogg',
+// 							ambient1:'http://localhost:8080/resources/sounds/ambient1.ogg', 
+// 							ambient2:'http://localhost:8080/resources/sounds/ambient2.ogg',
+// 							ambient3:'http://localhost:8080/resources/sounds/ambient3.ogg',
+// 							prisonDoor:'http://localhost:8080/resources/sounds/metal_barrier_open.ogg',
+// 							click:'http://localhost:8080/resources/sounds/click.ogg',
+// 							laugh:'http://localhost:8080/resources/sounds/laugh.ogg',
+// 							grab:'http://localhost:8080/resources/sounds/grab.ogg',
+// 							steps:'http://localhost:8080/resources/sounds/steps.ogg',
+// 							jump:'http://localhost:8080/resources/sounds/jump.ogg',
+// 							scream:'http://localhost:8080/resources/sounds/scream.ogg',
 
-							};
+// 							};
